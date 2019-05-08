cf32fc447c384365e6a5eb01035c16deb1c53feb
Pawel Cybulski <pawel.cybulski@naturaily.com>
2019-05-08 16:01:36 +0200

    Get estimate view

---
+++ _includes/estimate/estimate.js
@@ -0,0 +1,20 @@
+let vals = [];
+let valsString;
+
+function getValues () {
+
+  vals = [];
+  valsString = "";
+
+  jQuery("[data-estimate] :input:checked").each(function(index, item) {
+      vals.push(item.value);
+      valsString = vals.toString().replace(/\s/g, '');;
+  });
+}
+
+jQuery('[data-estimate=""]').on( "click", "input", function() {
+  getValues();
+  let formSource = "https://form.jotformeu.com/jsform/91251977229364?estimateFields=" + valsString;
+  jQuery("#input_9").val(valsString)
+  console.log(valsString);
+});
