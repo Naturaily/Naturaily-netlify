b4fda2d638b3d4239101b5d548fb31d6ff57c735
Pawel Cybulski <pawel.cybulski@naturaily.com>
2019-09-23 14:45:00 +0200

    scroll to + sticky nav

--- _includes/scroll-to-sticky.js
+++ _includes/scroll-to-sticky.js
@@ -1,30 +1,24 @@
-var sticky = new Waypoint.Sticky({
-  element: $('.section-navigation')[0]
+$(document).ready(function() {
+
+  var sticky = new Waypoint.Sticky({
+    element: $('.section-navigation')[0]
+  });
+
+  function getRelatedContent(el){
+    return $($(el).attr('href'));
+  }
+
+  $('.section-navigation a').on('click',function(e){
+    e.preventDefault();
+    $('html,body').animate({scrollTop:getRelatedContent(this).offset().top})
+  });
+
+  var waypoints = $('.section').waypoint({
+    handler: function(direction) {
+      var waypointNavItem = $('.section-navigation a[href=' + '"#' + (this.element.id) + '"' + ']')[0];
+      $('.section-navigation a').removeClass('is-active')
+      $(waypointNavItem).addClass('is-active');
+    }
+  })
+
 });
-
-function getRelatedContent(el){
-  return $($(el).attr('href'));
-}
-// Get link by section or article id
-function getRelatedNavigation(el){
-  console.log(el);
-  return $('.page-navigation_item[href=#'+$(el).attr('id')+']');
-}
-
-$('.page-navigation a').on('click',function(e){
-  e.preventDefault();
-  $('html,body').animate({scrollTop:getRelatedContent(this).offset().top - 20})
-});
-
-$('section').waypoint(function(direction) {
-
-   getRelatedNavigation(this).toggleClass('active', direction === 'down');
- }, {
-   offset: '90%' //
- })
- .waypoint(function(direction) {
-
-   getRelatedNavigation(this).toggleClass('active', direction === 'up');
- }, {
-   offset: function() {  return -$(this).height() + 100; }
- });
