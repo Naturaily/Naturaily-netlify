107dc0053ac725ababa81c08cbdf2fb80b211b06
Daniel Oskierko <daniel.oskierko@naturaily.com>
2020-04-21 08:50:49 +0200

    remove pn waves

--- _includes/tech-page-carousel.js
+++ _includes/tech-page-carousel.js
@@ -1,9 +1,11 @@
-$('#techCarousel').slick({
-  slidesToShow: 1,
-  autoplay: true,
-  autoplaySpeed: 4000,
-  pauseOnHover: true,
-  dots: false,
-  speed: 300,
-  arrows: true
+$(document).ready(() => {
+  $('#techCarousel').slick({
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: true,
+    dots: false,
+    speed: 300,
+    arrows: true
+  });
 });
