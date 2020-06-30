1cd76eb6211c9e55c3a80b61b83db39a24d0ea0d
galdranorn <skiberowska.justyna@wp.pl>
2020-06-30 14:20:56 +0200

    Html and css for hero section. Start to work on include section

--- _includes/carousel.js
+++ _includes/carousel.js
@@ -138,10 +138,22 @@ window.addEventListener('load', () => {
     }
   }
 
+<<<<<<< HEAD
   function changeTabPosition(newIndex) {
     const positionUnit = windowWidth > 576 && carousel.is_desktop ? '%' : 'vw';
     const newTabPosition = `-${newIndex * carousel.scale}${positionUnit}`;
     const newBarWidth = `${(newIndex + 1) * carousel.progress_bar_scale}%`;
+=======
+    function swipeControl(carouselSize) {
+      const swipeStartPosition = (event.type === 'touchstart') ? event.touches[0].clientX : event.clientX;
+      let targetTabIndex;
+
+      if (carousel.progress_bar) {
+        targetTabIndex = parseInt($carouselTabsContainer.find('[data-tab-active="true"]')[0].dataset.tabIndex);
+      } else {
+        targetTabIndex = parseInt($carouselDots.find('[data-dot-active="true"]')[0].dataset.dotIndex);
+      }
+>>>>>>> Html and css for hero section. Start to work on include section
 
     const blogCarousel = carousel.is_desktop && carousel.tabs_disabled;
     const includeCarousel = carousel.progress_bar;
@@ -153,11 +165,24 @@ window.addEventListener('load', () => {
       carouselClass = 'services-include';
     }
 
+<<<<<<< HEAD
     if (carouselClass) changeTabsAttributes(newIndex, carouselClass);
     if (includeCarousel) {
       $carouselBar.animate({ width: newBarWidth }, carousel.animationTime);
     } else {
       changeNavAttributes(newIndex);
+=======
+    function swiping(startPosition, tabIndex) {
+      $targetCarousel.on('mousemove touchmove', function() {
+        const currentPosition = (event.type === 'touchmove') ? event.touches[0].clientX : event.clientX;
+        const positionChange = currentPosition - startPosition;
+        const positionUnit = ($(window).width() > 576 && carousel.is_desktop) ? '%' : 'vw';
+        const newTabPosition = (positionChange < 0)
+          ? `calc(-${tabIndex*100}${positionUnit} - ${Math.abs(positionChange)}px)`
+          : `calc(-${tabIndex*100}${positionUnit} + ${positionChange}px)`
+        $carouselTabsContainer.css('left', newTabPosition);
+      });
+>>>>>>> Html and css for hero section. Start to work on include section
     }
 
     $carouselTabsContainer.animate({ left: newTabPosition }, carousel.animationTime, () => {
@@ -176,6 +201,7 @@ window.addEventListener('load', () => {
       tabsClass = 'custom-carousel__tabs-tab--inactive';
     }
 
+<<<<<<< HEAD
     $allTabs
       .addClass(tabsClass)
       .attr('data-tab-active', 'false');
@@ -216,6 +242,80 @@ window.addEventListener('load', () => {
       $newDisabledNav
         .attr('data-nav-disabled', 'true')
         .addClass(`${carouselClass}__nav-item--disabled`);
+=======
+    function checkChangeRequest(newIndex, type = 'default') {
+      let $activeElement;
+      let currentIndex;
+
+      if (carousel.progress_bar) {
+        $activeElement = $carouselTabsContainer.find('[data-tab-active="true"]');
+        currentIndex = parseInt($activeElement[0].dataset.tabIndex);
+      } else {
+        $activeElement = $carouselDots.find('[data-dot-active="true"]');
+        currentIndex = parseInt($activeElement[0].dataset.dotIndex);
+      }
+
+      if (!carousel.change_going && (newIndex !== currentIndex || type === 'swipe')) {
+        carousel.change_going = true;
+        changeTabPosition(newIndex, $activeElement);
+      }
+    }
+
+    function changeTabPosition(newIndex, $activeDot) {
+      const $newActiveDot = $carouselDots.find($(`[data-dot-index="${newIndex}"]`));
+      const positionUnit = ($(window).width() > 576 && carousel.is_desktop) ? '%' : 'vw'
+      const newTabPosition = `-${newIndex * 100}${positionUnit}`;
+      let $disabledNav = $targetCarousel.find('[data-nav-disabled="true"]');
+      let carouselClass;
+
+      if (carousel.is_desktop && carousel.tabs_disabled) {
+        carouselClass = 'custom-carousel';
+      } else if (carousel.is_desktop) {
+        carouselClass = 'carousel-partners';
+      } else {
+        carouselClass = 'custom-carousel';
+      }
+
+      if (carousel.is_desktop && carousel.tabs_disabled) {
+        const $allTabs = $carouselTabsContainer.find(`[data-carousel="tab"]`);
+        const $newActiveTab = $carouselTabsContainer.find($(`[data-tab-index="${newIndex}"]`));
+
+        $allTabs.addClass('webdevelopment-blog__posts-list__item--disabled');
+        $newActiveTab.removeClass('webdevelopment-blog__posts-list__item--disabled');
+      }
+
+      $disabledNav
+        .removeClass(`${carouselClass}__nav-item--disabled`)
+        .attr('data-nav-disabled', 'false');
+      $activeDot
+        .removeClass(`${carouselClass}__dots-item--active`)
+        .attr('data-dot-active', 'false');
+      $newActiveDot
+        .addClass(`${carouselClass}__dots-item--active`)
+        .attr('data-dot-active', 'true');
+
+
+      if (newIndex === 0) {
+        $disabledNav = $targetCarousel.find($(`[data-nav="prev"]`));
+      } else if (newIndex === (carouselSize - 1) ) {
+        $disabledNav = $targetCarousel.find($(`[data-nav="next"]`));
+      } else {
+        $disabledNav = null;
+      }
+
+      if ($disabledNav != null) {
+        $disabledNav.attr('data-nav-disabled', 'true')
+        if (carousel.is_desktop && !carousel.tabs_disabled) {
+          $disabledNav.addClass('carousel-partners__nav-item--disabled');
+        } else {
+          $disabledNav.addClass('custom-carousel__nav-item--disabled');
+        }
+      }
+
+      $carouselTabsContainer.animate({ left: newTabPosition }, 600, () => {
+        carousel.change_going = false;
+      });
+>>>>>>> Html and css for hero section. Start to work on include section
     }
   }
 }, { passive: true })
