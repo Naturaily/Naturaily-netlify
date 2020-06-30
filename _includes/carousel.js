d63dcd03b0293ffe984da42314b8652a364e653a
galdranorn <skiberowska.justyna@wp.pl>
2020-06-30 14:20:56 +0200

    Html and css for hero section. Start to work on include section

--- _includes/carousel.js
+++ _includes/carousel.js
@@ -1,15 +1,19 @@
 window.addEventListener('load', () => {
   const $carouselWrappers = $('[data-carousel="wrapper"]');
-  let slideChangeGoing = false;
-  let isCarouselDesktop;
-  let areTabsDisabled;
+  const carousel = {
+    change_going: false,
+    is_desktop: false,
+    tabs_disabled: false,
+    progress_bar: false
+  }
 
   $carouselWrappers.on('mousedown touchstart', () => {
     const $targetCarousel = $(event.target).closest($('[data-carousel="wrapper"]'));
-    isCarouselDesktop = $targetCarousel.data('carouselDesktop') ? true : false;
-    areTabsDisabled = $targetCarousel.data('tabsDisabled') ? true : false;
+    carousel.is_desktop = $targetCarousel.data('carouselDesktop') ? true : false;
+    carousel.tabs_disabled = $targetCarousel.data('tabsDisabled') ? true : false;
+    carousel.progress_bar = $targetCarousel.data('progressBar') ? true : false;
 
-    if ($(window).width() < 811 || isCarouselDesktop) {
+    if ($(window).width() < 811 || carousel.is_desktop) {
       initCarousel($targetCarousel);
     }
   });
@@ -20,7 +24,9 @@ window.addEventListener('load', () => {
     const targetType = event.target.dataset.carousel;
     let carouselSize;
 
-    if ($(window).width() < 811) {
+    if (carousel.progress_bar) {
+      carouselSize = $carouselTabsContainer.children().length;
+    } else if ($(window).width() < 811) {
       carouselSize = $carouselDots.children().length;
     } else if ($(window).width() > 992) {
       carouselSize = $carouselDots.find('[data-dots="lg"]').length;
@@ -53,8 +59,14 @@ window.addEventListener('load', () => {
     }
 
     function swipeControl(carouselSize) {
-      const targetTabIndex = parseInt($carouselDots.find('[data-dot-active="true"]')[0].dataset.dotIndex);
       const swipeStartPosition = (event.type === 'touchstart') ? event.touches[0].clientX : event.clientX;
+      let targetTabIndex;
+
+      if (carousel.progress_bar) {
+        targetTabIndex = parseInt($carouselTabsContainer.find('[data-tab-active="true"]')[0].dataset.tabIndex);
+      } else {
+        targetTabIndex = parseInt($carouselDots.find('[data-dot-active="true"]')[0].dataset.dotIndex);
+      }
 
       swiping(swipeStartPosition, targetTabIndex);
       $targetCarousel.one('mouseup touchend', () => {
@@ -66,7 +78,7 @@ window.addEventListener('load', () => {
       $targetCarousel.on('mousemove touchmove', function() {
         const currentPosition = (event.type === 'touchmove') ? event.touches[0].clientX : event.clientX;
         const positionChange = currentPosition - startPosition;
-        const positionUnit = ($(window).width() > 576 && isCarouselDesktop) ? '%' : 'vw';
+        const positionUnit = ($(window).width() > 576 && carousel.is_desktop) ? '%' : 'vw';
         const newTabPosition = (positionChange < 0)
           ? `calc(-${tabIndex*100}${positionUnit} - ${Math.abs(positionChange)}px)`
           : `calc(-${tabIndex*100}${positionUnit} + ${positionChange}px)`
@@ -86,44 +98,56 @@ window.addEventListener('load', () => {
     }
 
     function checkChangeRequest(newIndex, type = 'default') {
-      const $activeDot = $carouselDots.find('[data-dot-active="true"]');
-      const currentIndex = parseInt($activeDot[0].dataset.dotIndex);
+      let $activeElement;
+      let currentIndex;
 
-      if (!slideChangeGoing && (newIndex !== currentIndex || type === 'swipe')) {
-        slideChangeGoing = true;
-        changeTabPosition(newIndex, $activeDot);
+      if (carousel.progress_bar) {
+        $activeElement = $carouselTabsContainer.find('[data-tab-active="true"]');
+        currentIndex = parseInt($activeElement[0].dataset.tabIndex);
+      } else {
+        $activeElement = $carouselDots.find('[data-dot-active="true"]');
+        currentIndex = parseInt($activeElement[0].dataset.dotIndex);
+      }
+
+      if (!carousel.change_going && (newIndex !== currentIndex || type === 'swipe')) {
+        carousel.change_going = true;
+        changeTabPosition(newIndex, $activeElement);
       }
     }
 
     function changeTabPosition(newIndex, $activeDot) {
       const $newActiveDot = $carouselDots.find($(`[data-dot-index="${newIndex}"]`));
-      const positionUnit = ($(window).width() > 576 && isCarouselDesktop) ? '%' : 'vw'
+      const positionUnit = ($(window).width() > 576 && carousel.is_desktop) ? '%' : 'vw'
       const newTabPosition = `-${newIndex * 100}${positionUnit}`;
       let $disabledNav = $targetCarousel.find('[data-nav-disabled="true"]');
+      let carouselClass;
 
-      if (isCarouselDesktop && areTabsDisabled) {
+      if (carousel.is_desktop && carousel.tabs_disabled) {
+        carouselClass = 'custom-carousel';
+      } else if (carousel.is_desktop) {
+        carouselClass = 'carousel-partners';
+      } else {
+        carouselClass = 'custom-carousel';
+      }
+
+      if (carousel.is_desktop && carousel.tabs_disabled) {
         const $allTabs = $carouselTabsContainer.find(`[data-carousel="tab"]`);
         const $newActiveTab = $carouselTabsContainer.find($(`[data-tab-index="${newIndex}"]`));
 
         $allTabs.addClass('webdevelopment-blog__posts-list__item--disabled');
         $newActiveTab.removeClass('webdevelopment-blog__posts-list__item--disabled');
-        $disabledNav.removeClass('custom-carousel__nav-item--disabled');
-        $activeDot.removeClass('custom-carousel__dots-item--active');
-        $newActiveDot.addClass('custom-carousel__dots-item--active');
-      }
-      else if (isCarouselDesktop) {
-        $disabledNav.removeClass('carousel-partners__nav-item--disabled');
-        $activeDot.removeClass('carousel-partners__dots-item--active');
-        $newActiveDot.addClass('carousel-partners__dots-item--active');
-      } else {
-        $disabledNav.removeClass('custom-carousel__nav-item--disabled');
-        $activeDot.removeClass('custom-carousel__dots-item--active');
-        $newActiveDot.addClass('custom-carousel__dots-item--active');
       }
 
-      $disabledNav.attr('data-nav-disabled', 'false');
-      $activeDot.attr('data-dot-active', 'false');
-      $newActiveDot.attr('data-dot-active', 'true');
+      $disabledNav
+        .removeClass(`${carouselClass}__nav-item--disabled`)
+        .attr('data-nav-disabled', 'false');
+      $activeDot
+        .removeClass(`${carouselClass}__dots-item--active`)
+        .attr('data-dot-active', 'false');
+      $newActiveDot
+        .addClass(`${carouselClass}__dots-item--active`)
+        .attr('data-dot-active', 'true');
+
 
       if (newIndex === 0) {
         $disabledNav = $targetCarousel.find($(`[data-nav="prev"]`));
@@ -135,7 +159,7 @@ window.addEventListener('load', () => {
 
       if ($disabledNav != null) {
         $disabledNav.attr('data-nav-disabled', 'true')
-        if (isCarouselDesktop && !areTabsDisabled) {
+        if (carousel.is_desktop && !carousel.tabs_disabled) {
           $disabledNav.addClass('carousel-partners__nav-item--disabled');
         } else {
           $disabledNav.addClass('custom-carousel__nav-item--disabled');
@@ -143,7 +167,7 @@ window.addEventListener('load', () => {
       }
 
       $carouselTabsContainer.animate({ left: newTabPosition }, 600, () => {
-        slideChangeGoing = false;
+        carousel.change_going = false;
       });
     }
   }
