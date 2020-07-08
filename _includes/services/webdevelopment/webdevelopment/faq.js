a8d22b0000eeb78bace828010d285dd6a65aae12
galdranorn <skiberowska.justyna@wp.pl>
2020-07-08 10:58:06 +0200

    Add IoT consulting section

--- _includes/services/webdevelopment/webdevelopment/faq.js
+++ _includes/services/webdevelopment/webdevelopment/faq.js
@@ -1,27 +1,39 @@
 window.addEventListener('load', () => {
-  const $faqContainer = $('#services-faq');
-  const $faqTriggers = $faqContainer.find('[data-type="trigger"]');
+  const triggerAttribute = '[data-type="trigger"]';
+  const $faqTriggers = $(triggerAttribute);
+  let $faqContainer;
 
   $faqTriggers.click((e) => {
-    const triggeredIndex = e.target.dataset.index;
-    const $triggeredArrow = $(e.target).children();
-    const $triggeredContent = $faqContainer.find(`[data-content-index="${triggeredIndex}"]`);
-    const contentStatus = $triggeredContent[0].dataset.contentStatus;
+    $faqContainer = $(e.target.closest('[data-type="faq-container"]'));
+    const faqType = $faqContainer[0].dataset.faqType;
+    const mobileDevice = $(window).width() < 811;
+
+    if ((faqType === 'standard' && mobileDevice) || faqType === 'custom') runFaq(e);
+  });
+
+  function runFaq(e) {
+    const $triggeredElement = $(e.target.closest(triggerAttribute));
+
+    const index = $triggeredElement[0].dataset.index;
+    const $arrow = $triggeredElement.children().last();
+    const $content = $faqContainer.find(`[data-content-index="${index}"]`);
+
+    const contentStatus = $content[0].dataset.contentStatus;
 
     if (contentStatus === "visible") {
-      hideContent($triggeredContent, $triggeredArrow);
+      hideContent($content, $arrow);
     } else {
       const $visibleContent = $faqContainer.find(`[data-content-status="visible"]`);
-      const $rotatedArrow = $faqContainer.find(`[data-dot-status="rotated"]`);
-      
+      const $rotatedArrow = $faqContainer.find(`[data-arrow-status="rotated"]`);
+
       hideContent($visibleContent, $rotatedArrow);
-      showContent($triggeredContent, $triggeredArrow);
+      showContent($content, $arrow);
     }
-  })
+  }
 
   function hideContent($element, $arrow) {
     $arrow
-      .removeAttr('data-dot-status')
+      .removeAttr('data-arrow-status')
       .removeClass('services-faq__icon--rotated');
     $element
       .removeAttr('data-content-status')
@@ -30,7 +42,7 @@ window.addEventListener('load', () => {
 
   function showContent($element, $arrow) {
     $arrow
-      .attr('data-dot-status', 'rotated')
+      .attr('data-arrow-status', 'rotated')
       .addClass('services-faq__icon--rotated');
     $element
       .slideDown(600)
