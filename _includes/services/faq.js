411f2e56bb8d78057d25bf90659cac61f51485fd
galdranorn <skiberowska.justyna@wp.pl>
2020-12-03 11:34:02 +0100

    Style header desktop, refactor faq script

--- _includes/services/faq.js
+++ _includes/services/faq.js
@@ -1,32 +1,53 @@
 window.addEventListener('load', () => {
-  const triggerAttribute = '[data-type="trigger"]';
-  const $faqTriggers = $(triggerAttribute);
+  const faqAttr = {
+    trigger: '[data-type="trigger"]',
+    container: '[data-type="faq-container"]',
+    index: 'data-content-index',
+    arrow: 'data-arrow-status',
+    content: 'data-content-status'
+  };
+
+  const faqClasses = {
+    rotated: 'services-faq__icon--rotated',
+    hidden: 'services-faq__text--hidden'
+  };
+
+  const $faqTriggers = $(faqAttr.trigger);
   let $faqContainer;
 
   $faqTriggers.unbind('click').bind('click', (e) => {
-    $faqContainer = $(e.target.closest('[data-type="faq-container"]'));
-    const faqType = $faqContainer[0].dataset.faqType;
+    $faqContainer = $(e.target.closest(faqAttr.container));
     const mobileDevice = $(window).width() < 811;
+    const faqType = $faqContainer[0].dataset.faqType;
 
-    if ((faqType === 'standard' && mobileDevice) || faqType === 'custom') {
-      runFaq(e);
+    switch(faqType) {
+      case 'home':
+        faqClasses.rotated = 'home-hero__text-arrow--rotated';
+        faqClasses.hidden = 'home-hero__list--hidden';
+      case 'home':
+      case 'standard':
+        if (mobileDevice) { runFaq(e); }
+        break;
+      case 'custom':
+        runFaq(e);
+        break;
     }
   });
 
   const runFaq = (e) => {
-    const $triggeredElement = $(e.target.closest(triggerAttribute));
+    const $triggeredElement = $(e.target.closest(faqAttr.trigger));
 
     const index = $triggeredElement[0].dataset.index;
     const $arrow = $triggeredElement.children().last();
-    const $content = $faqContainer.find(`[data-content-index="${index}"]`);
+    const $content = $faqContainer.find(`[${faqAttr.index}="${index}"]`);
 
     const contentStatus = $content[0].dataset.contentStatus;
 
     if (contentStatus === "visible") {
       hideContent($content, $arrow);
     } else {
-      const $visibleContent = $faqContainer.find('[data-content-status="visible"]');
-      const $rotatedArrow = $faqContainer.find('[data-arrow-status="rotated"]');
+      const $visibleContent = $faqContainer.find(`[${faqAttr.content}="visible"]`);
+      const $rotatedArrow = $faqContainer.find(`[${faqAttr.arrow}="rotated"]`);
 
       hideContent($visibleContent, $rotatedArrow);
       showContent($content, $arrow);
@@ -35,20 +56,20 @@ window.addEventListener('load', () => {
 
   const hideContent = ($element, $arrow) => {
     $arrow
-      .removeAttr('data-arrow-status')
-      .removeClass('services-faq__icon--rotated');
+      .removeAttr(faqAttr.arrow)
+      .removeClass(faqClasses.rotated);
     $element
-      .removeAttr('data-content-status')
-      .slideUp(300, () => $element.addClass('services-faq__text--hidden'));
+      .removeAttr(faqAttr.content)
+      .slideUp(300, () => $element.addClass(faqClasses.hidden));
   };
 
   const showContent = ($element, $arrow) => {
     $arrow
-      .attr('data-arrow-status', 'rotated')
-      .addClass('services-faq__icon--rotated');
+      .attr(faqAttr.arrow, 'rotated')
+      .addClass(faqClasses.rotated);
     $element
       .slideDown(300)
-      .attr('data-content-status', 'visible')
-      .removeClass('services-faq__text--hidden');
+      .attr(faqAttr.content, 'visible')
+      .removeClass(faqClasses.hidden);
   };
 })
