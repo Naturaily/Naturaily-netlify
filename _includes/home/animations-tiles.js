13af47b410a91c53bc44517dadc9cf2a0b192ac3
galdranorn <skiberowska.justyna@wp.pl>
2020-12-16 10:04:23 +0100

    Add blog tiles animation and refactor js

--- _includes/home/animations-tiles.js
+++ _includes/home/animations-tiles.js
@@ -1,22 +1,28 @@
 const casesTilesTimeline    = gsap.timeline();
 const technologiesTimeline  = gsap.timeline();
+const blogTimeline          = gsap.timeline();
+const animationParams       = { duration: 0.6, opacity: 0, y: 50, stagger: .2, delay: .3 };
 
-const animationParams = { duration: 0.6, opacity: 0, y: 50, stagger: .2 }
+const tilesSections = {
+  cases:          { trigger: "[gsap-cases-tiles]", timeline: casesTilesTimeline },
+  technologies:   { trigger: "[gsap-technologies]", timeline: technologiesTimeline },
+  blog:           { trigger: "[gsap-blog]", timeline: blogTimeline }
+};
 
 casesTilesTimeline
   .from("[gsap-cases-tile]", animationParams);
 technologiesTimeline
   .from("[gsap-technologies-tile]", animationParams);
+blogTimeline
+  .from("[gsap-blog-background]", { duration: 1, width: 0 })
+  .from("[gsap-blog-tile]", animationParams, "-=.7")
+  .from("[gsap-blog-link]", { duration: 0.7, opacity: 0, scale: 0.9, y: 10, ease: "elastic" }, "+=.5");
 
-ScrollTrigger.create({
-  trigger: "[gsap-cases-tiles]",
-  animation: casesTilesTimeline,
-  toggleActions: "restart none restart none"
-});
-
-ScrollTrigger.create({
-  trigger: "[gsap-technologies]",
-  animation: technologiesTimeline,
-  markers: true,
-  toggleActions: "restart none restart none"
-});
+for (const section in tilesSections) {
+  ScrollTrigger.create({
+    trigger: tilesSections[section].trigger,
+    animation: tilesSections[section].timeline,
+    markers: true,
+    toggleActions: "restart none restart none"
+  });
+}
