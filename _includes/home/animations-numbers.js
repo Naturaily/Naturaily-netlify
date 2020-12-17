95b112db7e8affab29b354d7f9dbdc2e013dd2d1
galdranorn <skiberowska.justyna@wp.pl>
2020-12-17 13:36:32 +0100

    Make reverse when scroll up, refactor numbers animation

--- _includes/home/animations-numbers.js
+++ _includes/home/animations-numbers.js
@@ -1,48 +1,24 @@
 const numbersTimeline = gsap.timeline();
-const muchEarlier = "-=.7";
-const littleEarlier = "-=.35";
-const num = { var: 0 };
-
-function count() {
-  switch(this.vars.var) {
-    case 7:
-      counter1.innerHTML = (num.var).toFixed();
-      break;
-    case 60:
-      counter2.innerHTML = (num.var).toFixed();
-      break;
-    case 80:
-      counter3.innerHTML = `${(num.var).toFixed()}%`;
-      break;
-    case 125:
-      counter4.innerHTML = `${(num.var).toFixed()}k`
-      break;
-    case 0.8:
-      counter5.innerHTML = (num.var).toFixed(1);
-      break;
-  }
-};
+const num1 = { var: 0 };
+const num2 = { var: 0 };
+const num3 = { var: 0 };
+const num4 = { var: 0 };
+const num5 = { var: 0 };
 
 numbersTimeline
   .from("[gsap-numbers-background]", { duration: 1, width: 0 })
-  .set(num, { var: 0 }, muchEarlier)
-  .to(num, { var: 7, duration: .5, onUpdate: count }, muchEarlier)
-  .from("[gsap-numbers-tile-1]", { duration: .4, opacity: 0, y: 20 }, muchEarlier)
-  .set(num, { var: 50 }, littleEarlier)
-  .to(num, { var: 60, duration: .5, onUpdate: count }, littleEarlier)
-  .from("[gsap-numbers-tile-2]", { duration: .4, opacity: 0, y: 20 }, muchEarlier)
-  .set(num, { var: 60 }, littleEarlier)
-  .to(num, { var: 80, duration: .5, onUpdate: count }, littleEarlier)
-  .from("[gsap-numbers-tile-3]", { duration: .4, opacity: 0, y: 20 }, muchEarlier)
-  .set(num, { var: 100 }, littleEarlier)
-  .to(num, { var: 125, duration: .5, onUpdate: count }, littleEarlier)
-  .from("[gsap-numbers-tile-4]", { duration: .4, opacity: 0, y: 20 }, muchEarlier)
-  .set(num, { var: 0 }, littleEarlier)
-  .to(num, { var: 0.8, duration: .5, onUpdate: count }, littleEarlier)
-  .from("[gsap-numbers-tile-5]", { duration: .4, opacity: 0, y: 20 }, muchEarlier);
+  .from("[gsap-numbers-tile]", { duration: .7, opacity: 0, y: 30, stagger: .3 }, "-=.4")
+  .to(num1, { var: 7, duration: .5, onUpdate: () => counter1.innerHTML = num1.var.toFixed() }, "-=1.8")
+  .to(num2, { var: 60, duration: .5, onUpdate: () => counter2.innerHTML = num2.var.toFixed() }, "-=1.5")
+  .to(num3, { var: 80, duration: .5, onUpdate: () => counter3.innerHTML = `${num3.var.toFixed()}%` }, "-=1.2")
+  .to(num4, { var: 125, duration: .5, onUpdate: () => counter4.innerHTML = `${num4.var.toFixed()}k` }, "-=.9")
+  .to(num5, { var: 0.8, duration: .5, onUpdate: () => counter5.innerHTML = num5.var.toFixed(1) }, "-=.6");
 
 ScrollTrigger.create({
   trigger: "[gsap-numbers-tiles]",
   animation: numbersTimeline,
-  toggleActions: "restart none restart none"
+  end: "top+=125 bottom",
+  onEnter: () => numbersTimeline.timeScale(1).restart(),
+  onLeave: () => numbersTimeline.timeScale(1).play(),
+  onEnterBack: () => numbersTimeline.timeScale(2).reverse(2)
 });
