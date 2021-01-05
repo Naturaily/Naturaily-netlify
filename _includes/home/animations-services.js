dddaf4be834137d418f4e344cbd323af784a8ab5
galdranorn <skiberowska.justyna@wp.pl>
2021-01-05 12:31:46 +0100

    Add numbers image animations

--- _includes/home/animations-services.js
+++ _includes/home/animations-services.js
@@ -3,6 +3,7 @@ const servicesTimeline = gsap.timeline();
 servicesTimeline
   .from("[gsap-services-box]", { duration: .8, scaleX: 0 })
   .from("[gsap-services-image-mask]", { duration: .8, x: "100%" }, "-=.8")
+  .to("[gsap-numbers-accent]", { duration: .8, scaleX: 1, x: "50vw" }, "-=.8")
   .from("[gsap-services-image]", { duration: .8, x: 20 }, "-=.6")
   .from("[gsap-services-header]", { duration: .5, opacity: 0, x: 40 }, "-=.4")
   .from("[gsap-services-text]", { duration: .5, opacity: 0, y: 60 }, "-=.2")
