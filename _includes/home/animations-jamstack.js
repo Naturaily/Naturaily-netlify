ba98548b6304d44212754dccf91ec8f2529c00c5
galdranorn <skiberowska.justyna@wp.pl>
2021-01-11 18:08:55 +0100

    Add ScrollTrigger.matchmedia and resize behavior

--- _includes/home/animations-jamstack.js
+++ _includes/home/animations-jamstack.js
@@ -1,23 +1,27 @@
-const jamstackTimeline = gsap.timeline();
+ScrollTrigger.matchMedia({
+  "(min-width: 992px)": () => {
+    const jamstackTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "[gsap-jamstack]",
+        end: "top+=250 bottom",
+        onEnter: () => jamstackTimeline.restart(),
+        onLeave: () => jamstackTimeline.play(),
+        onEnterBack: () => jamstackTimeline.reverse(1)
+      }
+    });
 
-jamstackTimeline
-  .set("[gsap-jamstack-full]", { x: -190, y: 2 })
-  .set("[gsap-jamstack-half]", { opacity: 0 })
-  .to("[gsap-jamstack-mask]", { duration: 1, x: 600 })
-  .from("[gsap-jamstack-text]", { duration: .5, opacity: 0, x: 100 }, "-=.9")
-  .from("[gsap-jamstack-full]", { duration: .3, opacity: 0 }, "-=.5")
-  .from("[gsap-jamstack-half]", { duration: .01, opacity: 0 })
-  .from("[gsap-jamstack-full]", { duration: .5, x: -190 })
-  .from("[gsap-jamstack-tablet]", { duration: .5, x: -200, opacity: 0 }, "-=.5")
-  .from("[gsap-jamstack-page]", { duration: .5, x: 7, y: 30, opacity: 0 })
-  .from("[gsap-jamstack-cta]", { duration: .5, opacity: 0, y: 40 }, "-=.5")
-  .from("[gsap-jamstack-link]", { duration: 0.7, opacity: 0, scale: 0.8, y: 10 });
-
-
-ScrollTrigger.create({
-  trigger: "[gsap-jamstack]",
-  end: "top+=250 bottom",
-  onEnter: () => jamstackTimeline.restart(),
-  onLeave: () => jamstackTimeline.play(),
-  onEnterBack: () => jamstackTimeline.reverse(1)
+    jamstackTimeline
+      .set("[gsap-jamstack-full]", { x: -190, y: 2 })
+      .set("[gsap-jamstack-half]", { opacity: 0 })
+      .to("[gsap-jamstack-mask]", { duration: 1, x: 600 })
+      .from("[gsap-jamstack-text]", { duration: .5, opacity: 0, x: 100 }, "-=.9")
+      .from("[gsap-jamstack-full]", { duration: .3, opacity: 0 }, "-=.5")
+      .from("[gsap-jamstack-half]", { duration: .01, opacity: 0 })
+      .from("[gsap-jamstack-full]", { duration: .5, x: -190 })
+      .from("[gsap-jamstack-tablet]", { duration: .5, x: -200, opacity: 0 }, "-=.5")
+      .from("[gsap-jamstack-page]", { duration: .5, x: 7, y: 30, opacity: 0 })
+      .from("[gsap-jamstack-cta]", { duration: .5, opacity: 0, y: 40 }, "-=.5")
+      .from("[gsap-jamstack-link]", { duration: 0.7, opacity: 0, scale: 0.8, y: 10 })
+    ;
+  }
 });
--- _includes/home/animations-tiles.js
+++ _includes/home/animations-tiles.js
@@ -1,28 +1,47 @@
-const casesTilesTimeline = gsap.timeline();
-const technologiesTimeline = gsap.timeline();
-const blogTimeline = gsap.timeline();
+ScrollTrigger.matchMedia({
+  "(min-width: 992px)": () => {
+    const casesTilesTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "[gsap-cases-tiles]",
+        end: "top+=250 bottom",
+        onEnter: () => casesTilesTimeline.restart(),
+        onLeave: () => casesTilesTimeline.play(),
+        onEnterBack: () => casesTilesTimeline.reverse(1)
+      }
+    });
 
-const tilesSections = {
-  cases: { trigger: "[gsap-cases-tiles]", timeline: casesTilesTimeline },
-  technologies: { trigger: "[gsap-technologies]", timeline: technologiesTimeline },
-  blog: { trigger: "[gsap-blog]", timeline: blogTimeline }
-};
+    const technologiesTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "[gsap-technologies]",
+        end: "top+=250 bottom",
+        onEnter: () => technologiesTimeline.restart(),
+        onLeave: () => technologiesTimeline.play(),
+        onEnterBack: () => technologiesTimeline.reverse(1)
+      }
+    });
 
-casesTilesTimeline
-  .from("[gsap-cases-tile]", { duration: .8, opacity: 0, y: 40, stagger: .2 });
-technologiesTimeline
-  .from("[gsap-technologies-tile]", { duration: .7, opacity: 0, y: 70, stagger: .2 });
-blogTimeline
-  .from("[gsap-blog-background]", { duration: 1, backgroundPosition: "100% 0" })
-  .from("[gsap-blog-tile]", { duration: .6, opacity: 0, y: 50, stagger: .2 }, "-=.7")
-  .from("[gsap-blog-link]", { duration: .5, opacity: 0, scale: 0.8 }, "+=.3");
+    const blogTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "[gsap-blog]",
+        end: "top+=250 bottom",
+        onEnter: () => blogTimeline.restart(),
+        onLeave: () => blogTimeline.play(),
+        onEnterBack: () => blogTimeline.reverse(1)
+      }
+    });
 
-for (const section in tilesSections) {
-  ScrollTrigger.create({
-    trigger: tilesSections[section].trigger,
-    end: "top+=250 bottom",
-    onEnter: () => tilesSections[section].timeline.restart(),
-    onLeave: () => tilesSections[section].timeline.play(),
-    onEnterBack: () => tilesSections[section].timeline.reverse(1)
-  });
-}
+    casesTilesTimeline
+      .from("[gsap-cases-tile]", { duration: .8, opacity: 0, y: 40, stagger: .2 })
+    ;
+
+    technologiesTimeline
+      .from("[gsap-technologies-tile]", { duration: .7, opacity: 0, y: 70, stagger: .2 })
+    ;
+
+    blogTimeline
+      .from("[gsap-blog-background]", { duration: 1, backgroundPosition: "100% 0" })
+      .from("[gsap-blog-tile]", { duration: .6, opacity: 0, y: 50, stagger: .2 }, "-=.7")
+      .from("[gsap-blog-link]", { duration: .5, opacity: 0, scale: 0.8 }, "+=.3")
+    ;
+  }
+});
