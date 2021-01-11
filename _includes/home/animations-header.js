dd2b54754949807210a85c3a1cc2489c99a1809d
galdranorn <skiberowska.justyna@wp.pl>
2021-01-11 18:08:55 +0100

    Add ScrollTrigger.matchmedia and resize behavior

--- _includes/home/animations-header.js
+++ _includes/home/animations-header.js
@@ -1,15 +1,24 @@
-const headerTimeline = gsap.timeline();
+ScrollTrigger.matchMedia({
+  "(min-width: 992px)": () => {
+    const headerTimeline = gsap.timeline();
 
-headerTimeline
-  .from("[gsap-hero-background]", { duration: 1, backgroundPosition: "100% 0", delay: 1 })
-  .from("[gsap-logo]", { duration: .5, opacity: 0, ease: "back" }, "-=.7")
-  .from("[gsap-nav-item]", { duration: .5, opacity: 0, y: -150, stagger: 0.15 }, "-=.5")
-  .from("[gsap-eu-bar]", { duration: 1, opacity: 0, y: -100 }, "-=1")
-  .from("[gsap-nav-estimate]", { duration: .5, opacity: 0, ease: "back" }, "-=.5")
-  .from("[gsap-hero-line]", { duration: 0.7, opacity: 0, y: 20, stagger: .25 })
-  .from("[gsap-hero-mask]", { duration: .05, opacity: 0 }, "-=1.3")
-  .from("[gsap-hero-photo]", { duration: 4, opacity: 0, x: -50, scale: 0.9, ease: "expo" }, "-=1.3")
-  .from("[gsap-hero-clutch]", { duration: 1.5, opacity: 0, scale: 0.3, ease: "back" }, "-=3")
-  .from("[gsap-hero-star]", { duration: 0.7, opacity: 0, scale: 0.3, y: 20, stagger: .25, ease: "elastic" }, "-=3.2")
-  .from("[gsap-hero-areas]", { duration: 0.7, opacity: 0, left: -100 }, "-=2.3")
-  .from("[gsap-hero-tile]", { duration: 0.7, opacity: 0, y: 150, stagger: .15 }, "-=2");
+    headerTimeline
+      .from("[gsap-hero-background]", { duration: 1, backgroundPosition: "100% 0", delay: 1 })
+      .from("[gsap-logo]", { duration: .5, opacity: 0, ease: "back" }, "-=.7")
+      .from("[gsap-nav-item]", { duration: .5, opacity: 0, y: -150, stagger: 0.15 }, "-=.5")
+      .from("[gsap-eu-bar]", { duration: 1, opacity: 0, y: -100 }, "-=1")
+      .from("[gsap-nav-estimate]", { duration: .5, opacity: 0, ease: "back" }, "-=.5")
+      .from("[gsap-hero-line]", { duration: 0.7, opacity: 0, y: 20, stagger: .25 })
+      .from("[gsap-hero-mask]", { duration: .05, opacity: 0 }, "-=1.3")
+      .from("[gsap-hero-photo]", { duration: 4, opacity: 0, x: -50, scale: 0.9, ease: "expo" }, "-=1.3")
+      .from("[gsap-hero-clutch]", { duration: 1.5, opacity: 0, scale: 0.3, ease: "back" }, "-=3")
+      .from("[gsap-hero-star]", { duration: 0.7, opacity: 0, scale: 0.3, y: 20, stagger: .25, ease: "elastic" }, "-=3.2")
+      .from("[gsap-hero-areas]", { duration: 0.7, opacity: 0, left: -100 }, "-=2.3")
+      .from("[gsap-hero-tile]", { duration: 0.7, opacity: 0, y: 150, stagger: .15 }, "-=2")
+    ;
+
+    return function() {
+      headerTimeline.kill();
+    };
+  }
+});
--- _includes/home/animations-nondevelopment.js
+++ _includes/home/animations-nondevelopment.js
@@ -1,19 +1,24 @@
-const nondevelopmentTimeline = gsap.timeline();
+ScrollTrigger.matchMedia({
+  "(min-width: 992px)": () => {
+    const nondevelopmentTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "[gsap-nondevelopment]",
+        end: "top+=250 bottom",
+        onEnter: () => nondevelopmentTimeline.timeScale(1).restart(),
+        onLeave: () => nondevelopmentTimeline.timeScale(1).play(),
+        onEnterBack: () => nondevelopmentTimeline.timeScale(1.5).reverse(2)
+      }
+    });
 
-nondevelopmentTimeline
-  .from("[gsap-nondevelopment-icon-1]", { duration: .8, scale: .6, opacity: 0 })
-  .to("[gsap-nondevelopment-mask-1]", { duration: 1, x: 400 }, "-=.3")
-  .from("[gsap-nondevelopment-item-1]", { duration: .4, y: 10, opacity: 0, stagger: .2 }, "-=.8")
-  .from("[gsap-nondevelopment-link-1]", { duration: .5, opacity: 0, scale: .8 }, "-=.2")
-  .from("[gsap-nondevelopment-icon-2]", { duration: .8, scale: .6, opacity: 0 })
-  .to("[gsap-nondevelopment-mask-2]", { duration: 1, x: 400 }, "-=.3")
-  .from("[gsap-nondevelopment-item-2]", { duration: .4, y: 10, opacity: 0, stagger: .2 }, "-=.8")
-  .from("[gsap-nondevelopment-link-2]", { duration: .5, opacity: 0, scale: .8 }, "-=.2");
-
-ScrollTrigger.create({
-  trigger: "[gsap-nondevelopment]",
-  end: "top+=250 bottom",
-  onEnter: () => nondevelopmentTimeline.restart(),
-  onLeave: () => nondevelopmentTimeline.play(),
-  onEnterBack: () => nondevelopmentTimeline.timeScale(1.5).reverse(2)
+    nondevelopmentTimeline
+      .from("[gsap-nondevelopment-icon-1]", { duration: .8, scale: .6, opacity: 0 })
+      .to("[gsap-nondevelopment-mask-1]", { duration: 1, x: 400 }, "-=.3")
+      .from("[gsap-nondevelopment-item-1]", { duration: .4, y: 10, opacity: 0, stagger: .2 }, "-=.8")
+      .from("[gsap-nondevelopment-link-1]", { duration: .5, opacity: 0, scale: .8 }, "-=.2")
+      .from("[gsap-nondevelopment-icon-2]", { duration: .8, scale: .6, opacity: 0 })
+      .to("[gsap-nondevelopment-mask-2]", { duration: 1, x: 400 }, "-=.3")
+      .from("[gsap-nondevelopment-item-2]", { duration: .4, y: 10, opacity: 0, stagger: .2 }, "-=.8")
+      .from("[gsap-nondevelopment-link-2]", { duration: .5, opacity: 0, scale: .8 }, "-=.2")
+    ;
+  }
 });
