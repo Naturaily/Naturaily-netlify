eb854b3fbdd1a29ca90b824b9128541b1ab5e01d
galdranorn <skiberowska.justyna@wp.pl>
2021-01-14 16:57:10 +0100

    Fix links, remove dev hover, speed up nondev animation

--- _includes/home/animations-nondevelopment.js
+++ _includes/home/animations-nondevelopment.js
@@ -11,14 +11,14 @@ ScrollTrigger.matchMedia({
     });
 
     nondevelopmentTimeline
-      .from("[gsap-nondevelopment-icon-1]", { duration: .8, scale: .6, opacity: 0 })
+      .from("[gsap-nondevelopment-icon-1]", { duration: .6, scale: .6, opacity: 0 })
       .to("[gsap-nondevelopment-mask-1]", { duration: 1, x: 400 }, "-=.3")
       .from("[gsap-nondevelopment-item-1]", { duration: .4, y: 10, opacity: 0, stagger: .2 }, "-=.8")
-      .from("[gsap-nondevelopment-link-1]", { duration: .5, opacity: 0, scale: .8 }, "-=.2")
-      .from("[gsap-nondevelopment-icon-2]", { duration: .8, scale: .6, opacity: 0 })
+      .from("[gsap-nondevelopment-link-1]", { duration: .4, opacity: 0, scale: .8 }, "-=.2")
+      .from("[gsap-nondevelopment-icon-2]", { duration: .8, scale: .6, opacity: 0 }, "-=.7")
       .to("[gsap-nondevelopment-mask-2]", { duration: 1, x: 400 }, "-=.3")
       .from("[gsap-nondevelopment-item-2]", { duration: .4, y: 10, opacity: 0, stagger: .2 }, "-=.8")
-      .from("[gsap-nondevelopment-link-2]", { duration: .5, opacity: 0, scale: .8 }, "-=.2")
+      .from("[gsap-nondevelopment-link-2]", { duration: .4, opacity: 0, scale: .8 }, "-=.2")
     ;
   }
 });
