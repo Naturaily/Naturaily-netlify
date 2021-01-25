7a9dceff328a67e4bf6c375d2f93ccefe40d9f92
galdranorn <skiberowska.justyna@wp.pl>
2021-01-25 15:08:29 +0100

    Change jamstack figures from img to css

--- _includes/home/animations-jamstack.js
+++ _includes/home/animations-jamstack.js
@@ -18,13 +18,11 @@ ScrollTrigger.matchMedia({
     });
 
     jamstackTimeline
-      .set("[gsap-jamstack-full]", { x: -190, y: 2 })
-      .set("[gsap-jamstack-half]", { opacity: 0 })
       .to("[gsap-jamstack-mask]", { duration: 1, x: 600 })
       .from("[gsap-jamstack-text]", { duration: .5, opacity: 0, x: 100 }, "-=.9")
       .from("[gsap-jamstack-full]", { duration: .3, opacity: 0 }, "-=.5")
       .from("[gsap-jamstack-half]", { duration: .01, opacity: 0 })
-      .from("[gsap-jamstack-full]", { duration: .5, x: -190 })
+      .from("[gsap-jamstack-full]", { duration: .5, x: 190 })
       .from("[gsap-jamstack-tablet]", { duration: .5, x: -200, opacity: 0 }, "-=.5")
       .from("[gsap-jamstack-page]", { duration: .5, x: 7, y: 30, opacity: 0 })
       .from("[gsap-jamstack-cta]", { duration: .5, opacity: 0, y: 40 }, "-=.5")
