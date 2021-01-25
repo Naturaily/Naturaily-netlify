4c921d10d1614a415626c4a8544bf9babc31dca0
galdranorn <skiberowska.justyna@wp.pl>
2021-01-25 16:22:55 +0100

    Change newsletter figure from img to css, fix animation

--- _includes/home/animations-newsletter.js
+++ _includes/home/animations-newsletter.js
@@ -11,8 +11,6 @@ ScrollTrigger.matchMedia({
     });
 
     newsletterTimeline
-      .set("[gsap-newsletter-half]", { opacity: 0 })
-      .set("[gsap-newsletter-full]", { x: -100 })
       .from("[gsap-newsletter-form]", { duration: .8, opacity: 0, y: 30 })
       .from("[gsap-newsletter-btn]", { duration: .4, opacity: 0, scale: 0.8 })
       .from("[gsap-newsletter-text]", { duration: .4, color: "#312E2B" }, "-=.2")
@@ -21,7 +19,7 @@ ScrollTrigger.matchMedia({
       .to("[gsap-newsletter-type-name]", { text: { value: "Marcin Sulikowski" }, duration: .8, delay: 1, ease: "none" }, "-=.7")
       .from("[gsap-newsletter-full]", { duration: .6, opacity: 0 }, "-=1.4")
       .from("[gsap-newsletter-half]", { duration: .01, opacity: 0 }, "-=.8")
-      .from("[gsap-newsletter-full]", { duration: .8, x: -100 }, "-=.8")
+      .from("[gsap-newsletter-full]", { duration: .8, x: 100 }, "-=.8")
     ;
   }
 });
