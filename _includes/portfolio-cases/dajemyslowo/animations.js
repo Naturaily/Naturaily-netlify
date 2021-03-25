a8d2e08d3e13f4a79ec20c6e0cb82cd72631f6c3
galdranorn <skiberowska.justyna@wp.pl>
2021-03-25 10:46:59 +0100

    add challenge man gsap animation

--- _includes/portfolio-cases/dajemyslowo/animations.js
+++ _includes/portfolio-cases/dajemyslowo/animations.js
@@ -1,11 +1,11 @@
 const goalConfettiTimeline = gsap.timeline({ paused: true, repeat: -1, repeatDelay: 4 })
-  .to(`[data-gsap="picture"] [data-gsap="confetti"]`, { duration: .2, opacity: 0, stagger: .08 })
-  .to(`[data-gsap="picture"] [data-gsap="confetti"]`, { duration: .2, opacity: 1, stagger: .08 })
+  .to('[data-gsap="picture"] [data-gsap="confetti"]', { duration: .2, opacity: 0, stagger: .08 })
+  .to('[data-gsap="picture"] [data-gsap="confetti"]', { duration: .2, opacity: 1, stagger: .08 })
 ;
 
 const solutionConfettiTimeline = gsap.timeline({ paused: true, repeat: -1, repeatDelay: 4 })
-  .to(`[data-gsap="solution-people"] [data-gsap="confetti"]`, { duration: .2, opacity: 0, stagger: .1 })
-  .to(`[data-gsap="solution-people"] [data-gsap="confetti"]`, { duration: .2, opacity: 1, stagger: .1 })
+  .to('[data-gsap="solution-people"] [data-gsap="confetti"]', { duration: .2, opacity: 0, stagger: .1 })
+  .to('[data-gsap="solution-people"] [data-gsap="confetti"]', { duration: .2, opacity: 1, stagger: .1 })
 ;
 
 ScrollTrigger.matchMedia({
@@ -15,25 +15,30 @@ ScrollTrigger.matchMedia({
   },
 
   "(min-width: 992px)": () => {
-    const goalPictureTl = gsap.timeline({ scrollTrigger: { trigger: '[data-gsap="picture"]', markers: true } })
-      .from(`[data-gsap="picture"] [data-gsap="picture-hand-circle"]`, { duration: .4, scale: 0 })
-      .from(`[data-gsap="picture"] [data-gsap="picture-hand-chat"]`, { duration: .4, scale: 0 }, "-=.4")
-      .from(`[data-gsap="picture"] [data-gsap="picture-hand-hand"]`, { duration: .4, opacity: 0, x: -20, y: 30 })
-      .from(`[data-gsap="picture"] [data-gsap="picture-plus"]`, { duration: .4, scale: 0 })
-      .from(`[data-gsap="picture"] [data-gsap="picture-people-left"]`, { duration: .4, opacity: 0, x: -100 })
-      .from(`[data-gsap="picture"] [data-gsap="picture-people-right"]`, { duration: .4, opacity: 0, x: 100 }, "-=.4")
-      .from(`[data-gsap="picture"] [data-gsap="confetti"]`, { duration: .2, opacity: 0, stagger: .08 })
-      .from(`[data-gsap="picture"] [data-gsap="picture-equals"]`, { duration: .4, scale: 0 }, "-=1")
-      .from(`[data-gsap="picture"] [data-gsap="picture-logo"]`, { duration: .4, scale: 0, opacity: 0 })
+    gsap
+      .timeline({ scrollTrigger: { trigger: '[data-gsap="challenges"]', start: "0 center" } })
+      .from('[data-gsap="challenges-man"]', { duration: .6, x: 250, y: 650, ease: "power4.in" })
     ;
 
-    const solutionPeopleTl =  gsap.timeline({ scrollTrigger: { trigger: '[data-gsap="solution-people"]', markers: true } })
-      .from(`[data-gsap="solution-people"] [data-gsap="picture-people-left"]`, { duration: .4, opacity: 0, x: -150 })
-      .from(`[data-gsap="solution-people"] [data-gsap="picture-people-right"]`, { duration: .4, opacity: 0, x: 150 }, "-=.4")
-      .from(`[data-gsap="solution-people"] [data-gsap="confetti"]`, { duration: .2, opacity: 0, stagger: .1 })
+    const goalPictureTl = gsap.timeline({ scrollTrigger: { trigger: '[data-gsap="picture"]' } })
+      .from('[data-gsap="picture"] [data-gsap="picture-hand-circle"]', { duration: .4, scale: 0 })
+      .from('[data-gsap="picture"] [data-gsap="picture-hand-chat"]', { duration: .4, scale: 0 }, "-=.4")
+      .from('[data-gsap="picture"] [data-gsap="picture-hand-hand"]', { duration: .4, opacity: 0, x: -20, y: 30 })
+      .from('[data-gsap="picture"] [data-gsap="picture-plus"]', { duration: .4, scale: 0 })
+      .from('[data-gsap="picture"] [data-gsap="picture-people-left"]', { duration: .4, opacity: 0, x: -100 })
+      .from('[data-gsap="picture"] [data-gsap="picture-people-right"]', { duration: .4, opacity: 0, x: 100 }, "-=.4")
+      .from('[data-gsap="picture"] [data-gsap="confetti"]', { duration: .2, opacity: 0, stagger: .08 })
+      .from('[data-gsap="picture"] [data-gsap="picture-equals"]', { duration: .4, scale: 0 }, "-=1")
+      .from('[data-gsap="picture"] [data-gsap="picture-logo"]', { duration: .4, scale: 0, opacity: 0 })
     ;
-    
-    goalPictureTl.eventCallback("onComplete", () => goalConfettiTimeline.play());
-    solutionPeopleTl.eventCallback("onComplete", () => solutionConfettiTimeline.play());
+
+    const solutionPeopleTl =  gsap.timeline({ scrollTrigger: { trigger: '[data-gsap="solution-people"]' } })
+      .from('[data-gsap="solution-people"] [data-gsap="picture-people-left"]', { duration: .4, opacity: 0, x: -150 })
+      .from('[data-gsap="solution-people"] [data-gsap="picture-people-right"]', { duration: .4, opacity: 0, x: 150 }, "-=.4")
+      .from('[data-gsap="solution-people"] [data-gsap="confetti"]', { duration: .2, opacity: 0, stagger: .1 })
+    ;
+
+    goalPictureTl.eventCallback('onComplete', () => goalConfettiTimeline.play());
+    solutionPeopleTl.eventCallback('onComplete', () => solutionConfettiTimeline.play());
   }
 });
