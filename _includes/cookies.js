cea98ccae6cd1c930d0cda06a89be80db80ff866
Daniel Oskierko <daniel.oskierko@naturaily.com>
2021-05-07 17:11:03 +0200

    remove load listener from cookies script

--- _includes/cookies.js
+++ _includes/cookies.js
@@ -1,29 +1,27 @@
-window.addEventListener('load', () => {
-  const cookieAlert = document.getElementById('cookieInfo');
+const cookieAlert = document.getElementById('cookieInfo');
 
-  const setCookie = (name, value, days) => {
-    const date = new Date;
-    date.setTime(date.getTime() + 24 * 60 * 60 * 1000 * days);
+const setCookie = (name, value, days) => {
+  const date = new Date;
+  date.setTime(date.getTime() + 24 * 60 * 60 * 1000 * days);
 
-    document.cookie = `${name}=${value};path=/;expires=${date.toGMTString()}`;
-  };
+  document.cookie = `${name}=${value};path=/;expires=${date.toGMTString()}`;
+};
 
-  const getCookie = (name) => {
-    const v = document.cookie.match(`(^|;) ?${name}=([^;]*)(;|$)`);
+const getCookie = (name) => {
+  const v = document.cookie.match(`(^|;) ?${name}=([^;]*)(;|$)`);
 
-    return v ? v[2] : null;
-  };
+  return v ? v[2] : null;
+};
 
-  const toogleCoookieAlert = () => cookieAlert.classList.toggle('cookie-hide');
+const toogleCoookieAlert = () => cookieAlert.classList.toggle('cookie-hide');
 
-  if (!getCookie('cookiesAccepted')) {
-    const cookieBtnRemove = document.getElementById('cookieRemove');
+if (!getCookie('cookiesAccepted')) {
+  const cookieBtnRemove = document.getElementById('cookieRemove');
 
+  toogleCoookieAlert();
+
+  cookieRemove.addEventListener('click', () => {
+    setCookie('cookiesAccepted', true, 365);
     toogleCoookieAlert();
-
-    cookieRemove.addEventListener('click', () => {
-      setCookie('cookiesAccepted', true, 365);
-      toogleCoookieAlert();
-    })
-  }
-}, { passive: true });
+  })
+}
