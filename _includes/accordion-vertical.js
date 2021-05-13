db444648a87f87fb17bc8a0dfab59929d5509b91
galdranorn <skiberowska.justyna@wp.pl>
2021-05-13 11:34:38 +0200

    fix indentation

--- _includes/accordion-vertical.js
+++ _includes/accordion-vertical.js
@@ -1,33 +1,33 @@
 const accAttr = 'data-acc';
-  const accTargetAttr = `${accAttr}-target`;
-  const openClass = 'active';
+const accTargetAttr = `${accAttr}-target`;
+const openClass = 'active';
 
-  const openAccordion = (trigger, target, acc) => {
-    const activeInAcc = document.querySelectorAll(`[${accAttr}=${acc}].${openClass}`);
-    
-    [...activeInAcc].forEach((item) => item.classList.remove(openClass));
-    trigger.classList.add(openClass);
-    target.classList.add(openClass);
-  };
+const openAccordion = (trigger, target, acc) => {
+  const activeInAcc = document.querySelectorAll(`[${accAttr}=${acc}].${openClass}`);
+  
+  [...activeInAcc].forEach((item) => item.classList.remove(openClass));
+  trigger.classList.add(openClass);
+  target.classList.add(openClass);
+};
 
-  const closeAccordion = (trigger, target) => {
-    trigger.classList.remove(openClass);
-    target.classList.remove(openClass);
-  };
+const closeAccordion = (trigger, target) => {
+  trigger.classList.remove(openClass);
+  target.classList.remove(openClass);
+};
 
-  document.addEventListener('click', (e) => {
-    const accTrigger = e.target;
-    const accTriggerData = accTrigger.dataset.accTrigger;
-    const currentAcc = accTrigger.dataset.acc;
-    
-    if (accTriggerData) {
-      const accTarget = document.querySelector(`[${accTargetAttr}="${accTriggerData}"`);
-      const isAccOpened = accTarget.classList.contains('active');
+document.addEventListener('click', (e) => {
+  const accTrigger = e.target;
+  const accTriggerData = accTrigger.dataset.accTrigger;
+  const currentAcc = accTrigger.dataset.acc;
+  
+  if (accTriggerData) {
+    const accTarget = document.querySelector(`[${accTargetAttr}="${accTriggerData}"`);
+    const isAccOpened = accTarget.classList.contains('active');
 
-      if (isAccOpened) {
-        closeAccordion(accTrigger, accTarget);
-      } else {
-        openAccordion(accTrigger, accTarget, currentAcc);
-      }
+    if (isAccOpened) {
+      closeAccordion(accTrigger, accTarget);
+    } else {
+      openAccordion(accTrigger, accTarget, currentAcc);
     }
-  });
+  }
+});
